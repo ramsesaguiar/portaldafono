9333ac1c63f2ba0f7db3be2bf5e11b1896e726ac
Maikon Parreira <mknparreira@gmail.com>
2012-07-10 22:14:52 -0300

    teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portaldafono::Application.config.secret_token = '6e61706ca4e89e64a0922454db86377886ac77ee43c7bc9b18a88339f08c6f80bc8a9e01c2ee8100b44cea59f938d58fb42fb712dae319240a621a4e61d425d4'
