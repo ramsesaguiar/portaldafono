9713becc58e2d0782f878dcf8214843c5a500e21
mknparreira <maikonparreira@yahoo.com.br>
2012-10-21 21:01:43 -0200

    Layout do admin

--- app/controllers/admin/login_controller.rb
+++ app/controllers/admin/login_controller.rb
@@ -16,6 +16,7 @@
 			@user = Author.find_by_email_and_senha(user_email, user_senha)
 			if @user
 				session[:current_user_id] = @user.id
+				session[:nome] = @user.nome
 				if session[:return_to] && !session[:return_to].include?(url_for(:action => "index"))
 					redirect_to session[:return_to]
 					session[:return_to] = nil
@@ -31,6 +32,7 @@
 	
 	def logout
 		session[:current_user_id] = nil
+		session[:nome] = nil
 		#redirect_to root_url
 		redirect_to :controller => "login", :action => "index"
 	end
